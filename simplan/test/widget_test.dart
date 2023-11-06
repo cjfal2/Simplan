590a8cc93f71719019947bd966feecf9ac703cbe
cjfal2 <cjfal2@kakao.com>
2023-11-06 15:02:39 +0900

    수정: 메인 화면 띄우기 위한 수정

--- simplan/test/widget_test.dart
+++ simplan/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:simplan/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Simplan());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
